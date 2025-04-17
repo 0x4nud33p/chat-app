3c47c35e5484536200c29d697bf9c477d8c136c3
[anudeep avula] <anudeepavula009@gmail.com>
2025-04-17 17:23:50 +0530

    feat: add lib files

---
+++ lib/utils.ts
@@ -0,0 +1,30 @@
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: Date | string): string {
+  return new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  }).format(new Date(date));
+}
+
+export function formatFullDate(date: Date | string): string {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  }).format(new Date(date));
+}
+
+export function truncate(str: string, length: number): string {
+  if (str.length <= length) return str;
+  return `${str.slice(0, length)}...`;
+}

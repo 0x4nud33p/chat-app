1389b2c09917b1d568a442b7295f7db980f0041e
[anudeep avula] <anudeepavula009@gmail.com>
2025-06-30 16:03:00 +0530

    feat:prod deployment ready

--- utils/formatTime.ts
+++
@@ -1,7 +0,0 @@
-export const formatTime = (dateString: string) => {
-    const date = new Date(dateString);
-    return date.toLocaleTimeString("en-US", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-  };
